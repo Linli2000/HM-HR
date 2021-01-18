82bbefd555871df06a9caf20766cc38062f3830e
unknown <you@example.com>
2021-01-18 21:17:26 +0800

    自定义函数校验同级不能重名

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -29,6 +29,7 @@
 </template>
 
 <script>
+import { getDepartments } from '@/api/departments'
 export default {
   props: {
     showDialog: {
@@ -41,6 +42,29 @@ export default {
     }
   },
   data() {
+    const checkNameRepeat = async(rule, value, callback) => {
+      // 成功 直接调用 callback()
+      // 失败 调用 callback(new Error('错误原因'))
+      // 这里要校验当前输入名, 跟同一个父部门下的其他子部门没有重复
+      // 1. 获取所有部门数据
+      const res = await getDepartments()
+      // 2. 遍历所有的部门, 如果有一个部门 pid 跟我当前需要添加的父部门相同 item.pid === treeNode.id
+      // 3. 而且 item.name === value 就证明出错了
+
+      // 本来拿回来的是所有部门, 可以先用筛选, 筛选出属于当前父部门的数据
+      // 数组的过滤器可以实现, 过滤器的原理, 就是遍历所有的数据, 每个数据都执行一下回调
+      // 如果回调返回 true 就保留, 否则抛弃
+      // res.filter(item => item.pid === this.treeNode.id)
+      // 然后在这个基础上, 判断是否有同名
+      // 使用 some 方法, 可以判断一个数组当中, 是否存在某些元素符合规定
+      // 如果符合则返回 true 否则返回 false
+      const isRepeat = res.depts.filter(item => item.pid === this.treeNode.id).some(item => item.name === value)
+      if (isRepeat) {
+        callback(new Error('同一部门下, 不能有两个同名的子部门'))
+      } else {
+        callback()
+      }
+    }
     return {
       formData: {
         name: '',
@@ -52,7 +76,8 @@ export default {
       rules: {
         name: [
           { required: true, trigger: 'blur', message: '部门名称不能为空' },
-          { min: 1, max: 50, trigger: 'blur', message: '部门名称要求1到50个字符' }
+          { min: 1, max: 50, trigger: 'blur', message: '部门名称要求1到50个字符' },
+          { trigger: 'blur', validator: checkNameRepeat }
         ],
         code: [
           { required: true, trigger: 'blur', message: '部门代码不能为空' },
