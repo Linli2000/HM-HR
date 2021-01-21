707d4e067fc5cfbb34763b9f97406494ab62971a
unknown <you@example.com>
2021-01-21 15:25:19 +0800

    员工列表的数据渲染 和分页插件

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,52 +1,74 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container" />
-    <template>
-      <div class="dashboard-container">
-        <div class="app-container">
-          <page-tools :show-before="true">
-            <span slot="before">共166条记录</span>
-            <template slot="after">
-              <el-button size="small" type="warning">导入</el-button>
-              <el-button size="small" type="danger">导出</el-button>
-              <el-button size="small" type="primary">新增员工</el-button>
-            </template>
-          </page-tools>
-          <!-- 放置表格和分页 -->
-          <el-card>
-            <el-table border>
-              <el-table-column label="序号" sortable="" />
-              <el-table-column label="姓名" sortable="" />
-              <el-table-column label="工号" sortable="" />
-              <el-table-column label="聘用形式" sortable="" />
-              <el-table-column label="部门" sortable="" />
-              <el-table-column label="入职时间" sortable="" />
-              <el-table-column label="账户状态" sortable="" />
-              <el-table-column label="操作" sortable="" fixed="right" width="280">
-                <template>
-                  <el-button type="text" size="small">查看</el-button>
-                  <el-button type="text" size="small">转正</el-button>
-                  <el-button type="text" size="small">调岗</el-button>
-                  <el-button type="text" size="small">离职</el-button>
-                  <el-button type="text" size="small">角色</el-button>
-                  <el-button type="text" size="small">删除</el-button>
-                </template>
-              </el-table-column>
-            </el-table>
-            <!-- 分页组件 -->
-            <el-row type="flex" justify="center" align="middle" style="height: 60px">
-              <el-pagination layout="prev, pager, next" />
-            </el-row>
-          </el-card>
-        </div>
-      </div>
-    </template>
+    <page-tools :show-before="true">
+      <span slot="before">共 123 条记录</span>
+      <template slot="after">
+        <el-button size="small" type="warning">导入</el-button>
+        <el-button size="small" type="danger">导出</el-button>
+        <el-button size="small" type="primary">新增员工</el-button>
+      </template>
+    </page-tools>
+    <el-card>
+      <el-table border :data="list">
+        <el-table-column label="序号" sortable="" type="index" />
+        <el-table-column label="姓名" sortable="" prop="username" />
+        <el-table-column label="工号" sortable="" prop="workNumber" />
+        <el-table-column label="聘用形式" sortable="" prop="formOfEmployment" />
+        <el-table-column label="部门" sortable="" prop="departmentName" />
+        <el-table-column label="入职时间" sortable="" prop="timeOfEntry" />
+        <el-table-column label="账户状态" sortable="" prop="enableState" />
+        <el-table-column label="操作" sortable="" fixed="right" width="280">
+          <template>
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">转正</el-button>
+            <el-button type="text" size="small">调岗</el-button>
+            <el-button type="text" size="small">离职</el-button>
+            <el-button type="text" size="small">角色</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页组件 -->
+      <el-row type="flex" justify="center" align="middle" style="height: 60px">
+        <el-pagination layout="prev, pager, next" :total="page.total" @current-change="currentChange" />
+      </el-row>
+    </el-card>
   </div>
 </template>
 
 <script>
+import { getEmployeeList } from '@/api/employees'
 export default {
-
+  data() {
+    return {
+      // 接收数据
+      list: [],
+      page: {
+        page: 1,
+        size: 10,
+        total: 0
+      }
+    }
+  },
+  created() {
+    this.getEmployeeList()
+  },
+  methods: {
+    async getEmployeeList() {
+      // 结构数据
+      const { rows, total } = await getEmployeeList(this.page)
+      this.list = rows
+      console.log(this.list)
+      this.page.total = total
+    },
+    // 当用户点击翻页时会触发, 接收到用户选择的新页码
+    // 设定翻页数据, 重新加载数据即可
+    currentChange(newPage) {
+      this.page.page = newPage
+      this.getEmployeeList()
+    }
+  }
 }
 </script>
 
