628ec9022ba343c21fde285f481c4266de3bc0fa
Linli2000 <2627142284@qq.com>
2021-01-25 11:04:08 +0800

    全局注册上传组件, 和上传组件的默认显示布局

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- action 执行后端地址, 之后需要用腾讯云, 所以给一个 # 另外用 http-request 覆盖默认请求 -->
+    <el-upload
+      action="#"
+      :http-request="upload"
+
+      list-type="picture-card"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    upload() {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 // 该文件负责所有的公共的组件的全局注册   Vue.use
 import PageTools from './PageTools'
 import UploadExcel from '@/components/UploadExcel'
+import ImageUpload from '@/components/ImageUpload'
 
 export default {
   install(Vue) {
@@ -8,5 +9,7 @@ export default {
     Vue.component('PageTools', PageTools)
     // 注册导入excel组件
     Vue.component('UploadExcel', UploadExcel)
+    // 图片上传
+    Vue.component('ImageUpload', ImageUpload)
   }
 }
