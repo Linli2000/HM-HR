c9a8aac6c62c6d4ceb5646e9ec9b243e50bfd5b2
Linli2000 <2627142284@qq.com>
2021-01-25 11:05:58 +0800

    实现预览

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -6,17 +6,38 @@
       :http-request="upload"
 
       list-type="picture-card"
+      :on-preview="preview"
     >
       <i class="el-icon-plus" />
     </el-upload>
+    <!-- 预览弹窗 -->
+    <el-dialog title="预览" :visible.sync="showDialog">
+      <el-row type="flex" justify="center">
+        <img :src="previewUrl" alt="">
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      showDialog: false,
+      previewUrl: ''
+    }
+  },
   methods: {
     upload() {
 
+    },
+    // 预览函数
+    preview(file) {
+      console.log(file)
+      // 1. 将图片地址放入 img
+      this.previewUrl = file.url
+      // 2. 弹窗
+      this.showDialog = true
     }
   }
 }
