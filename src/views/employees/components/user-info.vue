1d3082a1c8b220a81ac8d8a2123438f63d9b36db
Linli2000 <2627142284@qq.com>
2021-01-26 14:18:43 +0800

    宣讲个人详情时, 使用上传组件并将旧图片作为默认显示

--- src/views/employees/components/user-info.vue
+++ src/views/employees/components/user-info.vue
@@ -59,7 +59,7 @@
         <el-col :span="12">
           <el-form-item label="员工头像">
             <!-- 放置上传图片 -->
-
+            <ImageUpload ref="staffPhoto" />
           </el-form-item>
         </el-col>
       </el-row>
@@ -90,6 +90,7 @@
         <!-- 员工照片 -->
 
         <el-form-item label="员工照片">
+          <ImageUpload ref="idPhoto" />
           <!-- 放置上传图片 -->
         </el-form-item>
         <el-form-item label="国家/地区">
@@ -365,6 +366,14 @@ export default {
   methods: {
     async getPersonalDetail() {
       this.formData = await getPersonalDetail(this.userId) // 获取员工数据
+      console.log(this.formData)
+      if (this.formData.staffPhoto) {
+        this.$refs.idPhoto.fileList = [
+          {
+            url: this.formData.staffPhoto
+          }
+        ]
+      }
     },
     async savePersonal() {
       await updatePersonal({ ...this.formData, id: this.userId })
@@ -377,6 +386,15 @@ export default {
     },
     async getUserDetailById() {
       this.userInfo = await getUserDetailById(this.userId)
+      // console.log(this.userInfo)
+      // 将 userInfo 的 staffPhoto 放入 子组件 fileList 当中即可显示
+      if (this.userInfo.staffPhoto) {
+        this.$refs.staffPhoto.fileList = [
+          {
+            url: this.userInfo.staffPhoto
+          }
+        ]
+      }
     }
   }
 }
