c4f77f9e219ca0860a2a6a4ab0576ecb627dca76
Linli2000 <2627142284@qq.com>
2021-01-28 20:24:47 +0800

    在角色页面添加弹窗

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,90 +1,135 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <template>
-        <div class="dashboard-container">
-          <div class="app-container">
-            <el-card>
-              <el-tabs>
-                <!-- 放置页签 -->
-                <el-tab-pane label="角色管理">
-                  <!-- 新增角色按钮 -->
-                  <el-row style="height:60px">
-                    <el-button
-                      icon="el-icon-plus"
-                      size="small"
-                      type="primary"
-                      @click="showDialog = true"
-                    >新增角色</el-button>
-                  </el-row>
-                  <!-- 表格 -->
-                  <el-table border="" :data="list">
-                    <!-- type="index"索引 从1开始 也可以作用域插槽  <template slot-scope="scope">
-                        {{ scope.$index +1 }}
-                      </template>-->
-                    <el-table-column label="序号" width="120" type="index" />
-                    <el-table-column label="角色名称" width="240" prop="name" />
-                    <el-table-column label="描述" prop="description" />
-                    <el-table-column label="操作">
-                      <template slot-scope="scope">
-                        <el-button size="small" type="success">分配权限</el-button>
-                        <el-button size="small" type="primary" @click="editRole(scope.row.id)">编辑</el-button>
-                        <el-button size="small" type="danger" @click="deleteRole(scope.row.id)">删除</el-button>
-                      </template>
-                    </el-table-column>
-                  </el-table>
-                  <!-- 分页组件 -->
-                  <el-row type="flex" justify="center" align="middle" style="height: 60px">
-                    <!-- 分页组件 -->
-                    <el-pagination :page-size="page.pagesize" :page-sizes="[2,5,10,20]" layout="total,sizes,prev,pager,next,jumper" :total="page.total" @current-change="currentChange" @size-change="sizeChange" />
-                  </el-row>
-                </el-tab-pane>
-                <el-tab-pane label="公司信息">
-                  <el-alert
-                    title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
-                    type="info"
-                    show-icon
-                    :closable="false"
-                  />
-                  <el-form label-width="120px" style="margin-top:50px">
-                    <el-form-item label="公司名称">
-                      <el-input v-model="formData.name" disabled style="width:400px" />
-                    </el-form-item>
-                    <el-form-item label="公司地址">
-                      <el-input v-model="formData.companyAddress" disabled style="width:400px" />
-                    </el-form-item>
-                    <el-form-item label="邮箱">
-                      <el-input v-model="formData.mailbox" disabled style="width:400px" />
-                    </el-form-item>
-                    <el-form-item label="备注">
-                      <el-input v-model="formData.remarks" type="textarea" :rows="3" disabled style="width:400px" />
-                    </el-form-item>
-                  </el-form>
-                </el-tab-pane>
-              </el-tabs>
-            </el-card>
-          </div>
-        </div>
-      </template>
-      <el-dialog title="编辑弹层" :visible="showDialog" @close="btnCancel">
-        <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px">
-          <el-form-item label="角色名称" prop="name">
-            <el-input v-model="roleForm.name" />
-          </el-form-item>
-          <el-form-item label="角色描述" prop="description">
-            <el-input v-model="roleForm.description" />
-          </el-form-item>
-        </el-form>
-        <!-- 底部 -->
-        <el-row slot="footer" type="flex" justify="center">
-          <el-col :span="6">
-            <el-button size="small" @click="btnCancel">取消</el-button>
-            <el-button size="small" type="primary" @click="btnOK">确定</el-button>
-          </el-col>
-        </el-row>
-      </el-dialog>
-
+      <el-card>
+        <el-tabs>
+          <!-- 放置页签 -->
+          <el-tab-pane label="角色管理">
+            <!-- 新增角色按钮 -->
+            <el-row style="height: 60px">
+              <el-button
+                icon="el-icon-plus"
+                size="small"
+                type="primary"
+                @click="showDialog = true"
+              >新增角色</el-button>
+            </el-row>
+            <!-- 表格 -->
+            <el-table border="" :data="list">
+              <el-table-column label="序号" width="120" type="index" />
+              <el-table-column label="角色名称" width="240" prop="name" />
+              <el-table-column label="描述" prop="description" />
+              <el-table-column label="操作">
+                <template slot-scope="{row}">
+                  <!-- scope.row -->
+                  <el-button size="small" type="success" @click="assignPerm(row.id)">分配权限</el-button>
+                  <el-button size="small" type="primary" @click="editRole(row.id)">编辑</el-button>
+                  <el-button size="small" type="danger" @click="deleteRole(row.id)">删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <!-- 分页组件 -->
+            <el-row
+              type="flex"
+              justify="center"
+              align="middle"
+              style="height: 60px"
+            >
+              <!-- 分页组件 -->
+              <el-pagination
+                :page-size="page.pagesize"
+                :page-sizes="[2, 5, 10, 20]"
+                layout="total,sizes,prev,pager,next,jumper"
+                :total="page.total"
+                @current-change="currentChange"
+                @size-change="sizeChange"
+              />
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="公司信息">
+            <el-alert
+              title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
+              type="info"
+              show-icon
+              :closable="false"
+            />
+            <el-form label-width="120px" style="margin-top: 50px">
+              <el-form-item label="公司名称">
+                <el-input
+                  v-model="formData.name"
+                  disabled
+                  style="width: 400px"
+                />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input
+                  v-model="formData.companyAddress"
+                  disabled
+                  style="width: 400px"
+                />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input
+                  v-model="formData.mailbox"
+                  disabled
+                  style="width: 400px"
+                />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input
+                  v-model="formData.remarks"
+                  type="textarea"
+                  :rows="3"
+                  disabled
+                  style="width: 400px"
+                />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
+    <el-dialog title="编辑弹层" :visible="showDialog" @close="btnCancel">
+      <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px">
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="roleForm.name" />
+        </el-form-item>
+        <el-form-item label="角色描述" prop="description">
+          <el-input v-model="roleForm.description" />
+        </el-form-item>
+      </el-form>
+      <!-- 底部 -->
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button size="small" @click="btnCancel">取消</el-button>
+          <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
+
+    <el-dialog title="分配权限" :visible="showPermDialog" @close="btnPermCancel">
+      <!-- 权限是一颗树 -->
+      <!-- 将数据绑定到组件上 -->
+      <!-- check-strictly 如果为true 那表示父子勾选时  不互相关联 如果为false就互相关联 -->
+      <!-- id作为唯一标识 -->
+      <!-- <el-tree
+        ref="permTree"
+        :data="permData"
+        :props="defaultProps"
+        :show-checkbox="true"
+        :check-strictly="true"
+        :default-expand-all="true"
+        :default-checked-keys="selectCheck"
+        node-key="id"
+      /> -->
+      <!-- 确定 取消 -->
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button type="primary" size="small" @click="btnPermOK">确定</el-button>
+          <el-button size="small" @click="btnPermCancel">取消</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
