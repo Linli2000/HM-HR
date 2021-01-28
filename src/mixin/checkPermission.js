3a2e6469bedb9f425f25bd3d1b279e6b8d1b4a75
Linli2000 <2627142284@qq.com>
2021-01-28 21:05:46 +0800

    实现全局混入的 checkpermission 方法

--- src/mixin/checkPermission.js
+++ src/mixin/checkPermission.js
@@ -1,5 +1,13 @@
+import store from '@/store'
+
 export default {
-  created() {
-    console.log('这里是混入的对象执行的钩子函数')
-  }
+  methods: {
+    checkPermission(key) {
+      // 遍历当前用户的 assignRoles.points 数组
+      // 如果key存在, 就是有权限, 返回 true, 否则 false
+      // 现在不在 vue 实例内, 拿不到 this
+      // store 需要靠引入
+      const { roles } = store.state.user.userInfo
+      return roles.points.indexOf(key) > -1
+    }
 }
