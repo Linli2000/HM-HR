004bf0e33ce89605ca603885ea709911f60edd3c
Linli2000 <2627142284@qq.com>
2021-01-28 20:26:42 +0800

    获取权限数据并且转换为树形结构

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -136,12 +136,14 @@
 <script>
 import { mapGetters } from 'vuex'
 import { getCompanyInfo, getRoleList, deleteRole, getRoleDetail, updateRole, addRole } from '@/api/setting'
-
+import { getPermissionList } from '@/api/permission'
+import { tranListToTreeData } from '@/utils'
 export default {
   data() {
     return {
       // 弹窗部分数据
       showDialog: false,
+      showPermDialog: false,
       roleForm: {
         name: '',
         description: ''
@@ -154,93 +156,107 @@ export default {
           { required: true, message: '角色描述不能为空', trigger: 'blur' }
         ]
       },
-      formData: {
-        // name: '',
-        // companyAddress: '',
-        // mailbox: '',
-        // remarks: ''
-      },
-      // 员工列表
+
+      // 存放员工数组
       list: [],
-      // 分页组件
+      // 存放公司信息
+      formData: {
+        name: 'dfadsf',
+        companyAddress: 'dsfdsfads',
+        mailbox: 'dsfsdaf',
+        remarks: 'dsfdsf'
+      },
+      // 创建一个专门用来管理翻页的对象
       page: {
-        pagesize: 5, // 每页显示的默认数据
-        page: 1, // 当前显示第几页
-        total: 0 // 总页数
-      }
+        pagesize: 2,
+        page: 1,
+        total: 0
+      },
+
+      permData: []
     }
   },
   computed: {
     ...mapGetters(['companyId'])
   },
   created() {
-    // console.log(11)
-    // 调用封装好的公司列表函数
-    this.getCompanyInfo()
-    // 获取员工列表
     this.getRoleList()
+    this.getCompanyInfo()
   },
   methods: {
-    // 获取的公司的信息
     async getCompanyInfo() {
       this.formData = await getCompanyInfo(this.companyId)
     },
-    // 获取员工信息
     async getRoleList() {
       const { rows, total } = await getRoleList(this.page)
       this.list = rows
+
       this.page.total = total
-      // console.log(this.list)
+      // 建议用下面的方法来改对象
+      // 好处在于每次修改其实都是创建一个新对象
+      // this.page = {
+      //   ...this.page,
+      //   total
+      // }
+
+      console.log(this.list)
     },
-    // 当前显示第几页
     currentChange(newPage) {
-      // console.log(11)
       // 1. 设定当前最新的请求数据(页码)
       this.page.page = newPage
       // 2. 重新发请求
       this.getRoleList()
     },
-    // 每页显示多少条数据 下拉框那里 ()括号
     sizeChange(newSize) {
-      // console.log(22)
       this.page.pagesize = newSize
       this.getRoleList()
     },
-    // deleteRole删除角色方法
     async deleteRole(id) {
       try {
+        // 可以考虑的优化
         // 如果删除之前发现
-        // 当前页只剩下一条数据 而且还不是第一页 就加载数据的时候向前翻一页
-        // 因为list只会拿到当前页码发送的数据并不会拿到所有的数据 拿到第几页有几条 他只会根据这个
+        // 当前页面剩下只有一条数据, 而且当前不是第一页
+        // 那么加载数据的时候往前翻一页
         if (this.list.length === 1 && this.page.page > 1) {
           this.page.page -= 1
         }
-        await deleteRole(id) // 调用删除接口
-        this.getRoleList() // 重新加载员工信息数据
+
+        // 1. 发送请求
+        await deleteRole(id)
+        // 2. 重新获取角色数据
+        this.getRoleList()
+
+        this.$message.success('删除成功')
       } catch (error) {
         console.log(error)
       }
     },
-    // 弹框的确定
+    async editRole(id) {
+      // 1.发送 api 请求获取表单数据
+      this.roleForm = await getRoleDetail(id)
+      this.showDialog = true
+    },
     async btnOK() {
-      // 点击弹框的确认时触发
-      // 校验表单
-      // 只有校验通过的情况下 才会执行await的下方内容
-      // roleForm这个对象有id就是编辑 没有id就是新增
+      // 点击确认时触发
+      // 1. 校验表单
       await this.$refs.roleForm.validate()
+
+      // 2. 发送数据
+      // 发送时需要区分到底是编辑还是新增, 根据不同状况,发送不同 api 请求
       if (this.roleForm.id) {
-        // 发送数据 修改角色 发送请求 带着新的后台需要的数据
+        // 编辑
         await updateRole(this.roleForm)
       } else {
-        // 新增业务
+        // 新增
         await addRole(this.roleForm)
       }
-      // 3. 重新加载数据, 关闭弹窗
+
+      // 3. 后续处理(重新加载数据, 关闭弹窗)
       this.getRoleList()
       this.showDialog = false
-      this.$message.success('修改成功')
+
+      this.$message.success('操作成功')
     },
-    // 弹框的取消
     btnCancel() {
       // 1. 清理数据恢复数据对象
       this.roleForm = {
@@ -252,18 +268,27 @@ export default {
       // 3. 关闭弹窗
       this.showDialog = false
     },
-    // editRole编辑功能
-    async editRole(id) {
-      // console.log(111)
-      // 发送api请求获取表单数据 获取原始的角色详情
-      this.roleForm = await getRoleDetail(id)
-      // console.log(this.roleForm)
-      this.showDialog = true
-    }
+
+    async assignPerm(id) {
+      // 1. 回显数据
+      // 1.1 回显全部的权限数据
+      // 获取后需要转换成树形数据
+      // 这个转换函数需要传入两个参数
+      //  1. 原数组(平铺并列)
+      //  2. 默认遍历源头(没有任何上一层的状态, 对于权限来说, '0')
+      const data = await getPermissionList()
+      this.permData = tranListToTreeData(data, '0')
+      console.log(data)
+      console.log(this.permData)
+      // 1.2 回显当前角色的权限
+      // 2. 弹出弹窗
+      this.showPermDialog = true
+    },
+    btnPermOK() {},
+    btnPermCancel() {}
   }
 }
 </script>
 
 <style>
-
 </style>
