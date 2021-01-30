5e8c3c0f8a0fdd9bb0e34c368bde9bf2bf71b8cf
Linli2000 <2627142284@qq.com>
2021-01-30 11:23:27 +0800

    user 路由引入到静态路由中

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ Vue.use(Router)
 
 /* Layout */
 import Layout from '@/layout'
-
+import UserRoute from './modules/user'
 /**
  * Note: sub-menu only appear when route children.length >= 1
  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
@@ -31,6 +31,7 @@ import Layout from '@/layout'
  * all roles can be accessed
  */
 export const constantRoutes = [
+  UserRoute,
   {
     path: '/login',
     component: () => import('@/views/login/index'),
